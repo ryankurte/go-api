256fba9749cc9151b1fe16938cfcfe544f4f052e
Ryan Kurte <ryankurte@gmail.com>
2018-02-09 16:14:48 +1300

    Updated options

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 ## Status
 
+
+[![Build Status](https://travis-ci.org/ryankurte/go-api.-buildersvg?branch=master)](https://travis-ci.org/ryankurte/go-api-builder)
+[![Documentation](https://img.shields.io/badge/docs-godoc-blue.svg)](https://godoc.org/github.com/ryankurte/go-api-builder)
+[![Release](https://img.shields.io/github/release/ryankurte/go-api-builder.svg)](https://github.com/ryankurte/go-api-builder)
+
+
 ## Overview
 
 - [handlers](handlers) provide base server handling (ie. http server, AWS lambda)
--- lib/options/options.go
+++ lib/options/options.go
@@ -1,12 +1,19 @@
 package options
 
-// Options are base API server options
-type Options struct {
-	Mode string `short:"m" long:"mode" description:"Server mode" option:"server" option:"lambda"`
+// Base are base API server options
+type Base struct {
+	Mode string `short:"m" long:"mode" description:"Server mode" option:"http" option:"lambda" default:"http"`
 
 	BindAddress string `short:"b" long:"address" description:"Address to bind API server" default:"0.0.0.0"`
 	Port        string `short:"p" long:"port" description:"Port on which to bind API server" default:"10001"`
 
+	NoTLS   bool   `long:"no-tls" description:"Disable TLS"`
 	TLSCert string `short:"c" long:"tls-cert" description:"TLS certificate file"`
 	TLSKey  string `short:"k" long:"tls-key" description:"TLS key file"`
 }
+
+// Server mode constants
+const (
+	ModeLambda = "lambda"
+	ModeHTTP   = "http"
+)
